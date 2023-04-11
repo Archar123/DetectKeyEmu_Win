c874baf6d1b4c1fcd25adcddf5eeef70dea99f9b
huanjun.zhu <huanjun.zhu@mihoyo.com>
2023-04-11 11:53:58 +0800

    +

--- AntiEmulation_Win/SendInput/sendinput_io.c
+++ AntiEmulation_Win/SendInput/sendinput_io.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 #include "mouse.h"
+HHOOK hook;
+
+MSLLHOOKSTRUCT mouseStruct;
+
+LRESULT CALLBACK LLMP(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode >= 0)
+	{
+		mouseStruct = *((MSLLHOOKSTRUCT*)lParam);
+
+		if ((mouseStruct.flags & LLMHF_INJECTED) != 0)
+			mouseStruct.flags &= ~LLMHF_INJECTED;
+
+		if ((mouseStruct.flags & LLMHF_LOWER_IL_INJECTED) != 0)
+			mouseStruct.flags &= ~LLMHF_LOWER_IL_INJECTED;
+	}
+
+	return CallNextHookEx(hook, nCode, wParam, lParam);
+}
+
 
 int main(int argc, char *argv[])
 {
-	
-	HWND hwnd = FindWindowW(NULL, L"原神");
-	if (hwnd == NULL)
+	if (!(hook = SetWindowsHookEx(WH_MOUSE_LL, LLMP, NULL, 0)))
 	{
-		printf("get hwnd failed, %d", GetLastError());
-		getchar();
-		return 0;
+		MessageBox(NULL, L"failed to install hook", L"fail", MB_ICONERROR);
+	}
+
+	//printf("press any key to emun. \r\n");
+	//getchar();
+
+	HWND hwnd = NULL;
+
+	while (hwnd == NULL)
+	{
+		hwnd = FindWindowW(NULL, L"原神");
+		Sleep(1000);
 	}
 
     if (!SetForegroundWindow(hwnd))
@@ -35,5 +62,6 @@ int main(int argc, char *argv[])
 		Sleep(5000);
 	}
 
+	UnhookWindowsHookEx(hook);
 	return 0;
 }
